ed2cb23b5921d089f30f711ff737095511604ef1
MeetKadivar <meetkadivar88@gmail.com>
2021-08-02 16:08:08 +0530

    [Fix]

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?
 include_once("index.html");
+include_once("product1.html");
+include_once("product2.html");
+include_once("product3.html");
+include_once("product4.html");
+include_once("product5.html");
+include_once("product6.html");
+include_once("about.html");
+include_once("contact.html");
+
 ?>
